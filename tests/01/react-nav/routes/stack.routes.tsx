c20606f7dca03666e26256e986387d953ac86843
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2025-11-24 09:54:59 -0300

    feat: completing the code of routes/stack.routes.tsx in the test project

--- tests/01/react-nav/routes/stack.routes.tsx
+++ tests/01/react-nav/routes/stack.routes.tsx
@@ -1,3 +1,15 @@
 import {createStackNavigator} from  '@react-navigation/stack'
 
 const { Screen, Navigator } = createStackNavigator()
+
+import HomeScreen from '../src/screens/HomeScreen'
+import GalleryScreen from '../src/screens/GalleryScreen'
+
+export function StackRoutes() {
+    return (
+        <Navigator>
+            <Screen name='home' component={HomeScreen} />
+            <Screen name='gallery' component={GalleryScreen} />            
+        </Navigator>
+    )    
+}
