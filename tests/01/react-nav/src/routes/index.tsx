22da85d830da2632c6f4a4bf4a62f35a75257930
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2025-11-25 09:03:34 -0300

    test: src/routes/index.tsx

---
+++ tests/01/react-nav/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native'
+
+// contexto
+import { StackRoutes } from "./stack.routes"
+
+// caixa de contextos
+export function Routes {
+    <NavigationContainer>
+        <StackRoutes />
+    </NavigationContainer>
+}
